9bf4784e52260103dd18e359374abc63babc3158
kieucumber22 <khuynh5184@gmail.com>
2019-11-10 09:19:43 -0500

    adding scenes

---
+++ GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject HintMenu;
+    public Button HintMenuButton;
+    public Button CloseMenuButton;
+
+
+
+    void Start(){
+        Button btn = HintMenuButton.GetComponent<Button>();
+        Button btn2 = CloseMenuButton.GetComponent<Button>();
+        HintMenuButton.onClick.AddListener(OpenMenu);
+        CloseMenuButton.onClick.AddListener(CloseMenu);
+    }
+
+    void OpenMenu(){
+        
+        HintMenu.gameObject.SetActive(true);
+        HintMenuButton.gameObject.SetActive(false);
+        CloseMenuButton.gameObject.SetActive(true);
+    }
+    void CloseMenu() {
+        HintMenu.gameObject.SetActive(false);
+        HintMenuButton.gameObject.SetActive(true);
+        CloseMenuButton.gameObject.SetActive(false);
+    }
+
+}
---
+++ SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("AR");
+    }
+}
